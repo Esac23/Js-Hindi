b6aa99142563f1e27950c67435e0b1fc6595de90
Esac23 <118606255+Esac23@users.noreply.github.com>
2024-08-07 07:31:45 +0000

    done

---
+++ 01_basics/03_variable.js
@@ -0,0 +1,23 @@
+let score=null
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//'33' => string
+//'33abc' => output is NaN but type ishka number hi rahega
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = "pp"
+console.log(isLoggedIn)
+let booleanInLoggedIn = Boolean(0)
+console.log(booleanInLoggedIn) 
+//1 => true; 0=>false
+//"" => false
+
+let someNumber = 33
+let someString =String(someNumber)
+console.log(typeof someString)
+

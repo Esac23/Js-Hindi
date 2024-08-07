91459441159bfadecacfccfa96284edc4693632e
Esac23 <118606255+Esac23@users.noreply.github.com>
2024-08-07 07:02:20 +0000

    saved

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountID = 1111
+let accountEmail="priyankaprakash410@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+console.log(accountID)
+console.table([accountID, accountEmail, accountPassword, accountCity,accountState])
+// alert(3+3) we are usig node js not browser
+console.log(3+3)
+
+console.log("hello")
+console.log(typeof "hello")
+console.log(typeof undefined)//output is undefined
+console.log(typeof null)//output is object
